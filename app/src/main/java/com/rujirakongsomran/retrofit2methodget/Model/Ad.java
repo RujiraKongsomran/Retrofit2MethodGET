429e415ad509bc3fa4f18eefd3009af693908300
Rujira Kongsomran <biwberry@outlook.com>
2020-05-19 18:48:45 +0700

    Create Model

---
+++ app/src/main/java/com/rujirakongsomran/retrofit2methodget/Model/Ad.java
@@ -0,0 +1,37 @@
+package com.rujirakongsomran.retrofit2methodget.Model;
+
+public class Ad {
+    private String company;
+    private String url;
+    private String text;
+
+    public Ad(String company, String url, String text) {
+        this.company = company;
+        this.url = url;
+        this.text = text;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
