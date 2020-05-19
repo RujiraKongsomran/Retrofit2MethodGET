39183502f021a48b473d94f4627951161783f4e8
Rujira Kongsomran <biwberry@outlook.com>
2020-05-19 19:28:38 +0700

    Call Service

--- app/src/main/java/com/rujirakongsomran/retrofit2methodget/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/retrofit2methodget/MainActivity.java
@@ -3,12 +3,55 @@ package com.rujirakongsomran.retrofit2methodget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.rujirakongsomran.retrofit2methodget.Model.Users;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://reqres.in/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<Users> call = jsonPlaceHolderApi.getListUsers();
+        call.enqueue(new Callback<Users>() {
+            @Override
+            public void onResponse(Call<Users> call, Response<Users> response) {
+                if (!response.isSuccessful()) {
+                    tvResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                Users users = response.body();
+
+            }
+
+            @Override
+            public void onFailure(Call<Users> call, Throwable t) {
+
+            }
+        });
+
     }
 }
